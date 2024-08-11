ea52da52c96b4a76476481025f8692bb913d4c38
Prasannarajpaul <mprasannarajpaul@gmail.com>
2024-08-11 22:32:24 +0530

    Chapter 1 updated

---
+++ Chapter 1/05_comments.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main(){
+    //I am writing this for fun. This line doesnt do anything
+    printf("Hello Wolrd");
+
+    //Comments 
+    //Used to clarify about the program in plain language. It is used for notes to out program. There are two types of comments in C.
+    //Comments in C program is not executed and are ignored.
+
+    //Single line Comment
+    // using double forward slashes (//) before anything will be excluded.
+
+    /*
+    Multiline Comment
+    like this used to write more than one line
+    starts with (forward slash + asterisk)   and ends with (asterisk + forward slash)
+    */
+
+
+
+    //Compilation and Execution
+    // first.c --> C Compiler --> first.exe
+    // in vs code   **gcc
+    return 0;
+}
